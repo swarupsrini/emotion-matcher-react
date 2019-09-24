269411332ed88e10ceeaf6531c829db0778411f7
Swarup Srini <gswarup2001@gmail.com>
2019-09-24 01:36:42 -0400

    Updates

---
+++ precache-manifest.47c41c8cdaee768dfb5da917bafadedd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b257977fb2338e8bfd34c70aea2d31",
+    "url": "/emotion-matcher-react/index.html"
+  },
+  {
+    "revision": "e392f0e858dd80bc40ec",
+    "url": "/emotion-matcher-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "392ae2c00772ad1201ba",
+    "url": "/emotion-matcher-react/static/js/2.40fc6bc1.chunk.js"
+  },
+  {
+    "revision": "e392f0e858dd80bc40ec",
+    "url": "/emotion-matcher-react/static/js/main.3f644b65.chunk.js"
+  },
+  {
+    "revision": "1dcafed79511120af67f",
+    "url": "/emotion-matcher-react/static/js/runtime-main.d375b250.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/emotion-matcher-react/static/media/logo.25bf045c.svg"
+  }
+]);
